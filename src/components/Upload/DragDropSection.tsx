93ff4277da043c4bd70e80b38b9548a09e1b4e00
LordRonz <aarontanhar2000@gmail.com>
2022-06-15 20:20:23 +0700

    feat: drag drop rocket:

--- src/components/Upload/DragDropSection.tsx
+++ src/components/Upload/DragDropSection.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { useState } from 'react';
 import { FileUploader } from 'react-drag-drop-files';
 
-const DragDropSection = () => {
-  const [file, setFile] = useState<File[] | null>(null);
-  const handleChange = (file: File[]) => {
+type DragDropSectionProps = {
+  name?: string;
+  title?: string;
+  multiple?: boolean;
+  note?: string;
+  types?: string[];
+};
+
+const SUPPORTED_IMAGE_TYPES = ['jpeg', 'jpg', 'png', 'webp'];
+
+const DragDropSection = ({
+  name = 'file',
+  title = 'Drop your artwork here',
+  multiple = false,
+  note = 'Allowed all format | Max 1 GB',
+  types = SUPPORTED_IMAGE_TYPES,
+}: DragDropSectionProps) => {
+  const [file, setFile] = useState<File[] | File | null>(null);
+  const handleChange = (file: File[] | File) => {
     setFile(file);
   };
 
@@ -14,11 +30,21 @@ const DragDropSection = () => {
         <div className='browse-file-icon'>
           <i className='flaticon-cloud-computing'></i>
         </div>
-        <h1 className='browse-file-text'>Drop your artwork here</h1>
-        <FileUploader multiple={true} handleChange={handleChange} name='file' />
-        <div className='browse-file-note'>Allowed all format | Max 1 GB</div>
+        <h1 className='browse-file-text'>{title}</h1>
+        <FileUploader
+          multiple={multiple}
+          handleChange={handleChange}
+          name={name}
+          types={types}
+          hoverTitle='Drop here'
+        />
+        <div className='browse-file-note'>{note}</div>
       </div>
-      <p>{file ? `File name: ${file?.[0].name}` : 'no files uploaded yet'}</p>
+      <p>
+        {file
+          ? `File name: ${Array.isArray(file) ? file?.[0].name : file.name}`
+          : 'no files uploaded yet'}
+      </p>
     </>
   );
 };
