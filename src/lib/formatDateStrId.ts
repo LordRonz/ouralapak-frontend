aeca88e3c323ba63e4dbadc41dca87e05c6cc9de
LordRonz <aarontanhar2000@gmail.com>
2022-08-14 11:00:13 +0700

    feat: add content loader :rocket:

--- src/lib/formatDateStrId.ts
+++ src/lib/formatDateStrId.ts
@@ -2,10 +2,15 @@ import id from 'date-fns/locale/id';
 import { formatInTimeZone } from 'date-fns-tz';
 
 export const formatDateStrId = (
-  date: string,
+  date: string | number,
   format = 'dd/MM/yyyy HH:mm zzz'
 ) => {
-  return formatInTimeZone(date, 'Asia/Jakarta', format, { locale: id });
+  return formatInTimeZone(
+    typeof date === 'number' ? date * 1000 : date,
+    'Asia/Jakarta',
+    format,
+    { locale: id }
+  );
 };
 
 export default formatDateStrId;
