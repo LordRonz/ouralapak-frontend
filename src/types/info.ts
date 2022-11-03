b013cd11275affc9a423c0f05f2c3d1f62748f9e
LordRonz <aarontanhar2000@gmail.com>
2022-11-03 21:26:05 +0700

    feat: deez nutz amogus :rocket:

---
+++ src/types/info.ts
@@ -0,0 +1,10 @@
+export type Info = {
+  attribute: string;
+  created_at: number;
+  created_by: number;
+  id: number;
+  info: string;
+  name: string;
+  updated_at: string;
+  updated_by: number | null;
+};
